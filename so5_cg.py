ecf30ba8fd8fcd4ca521df0e855d050fcb4db409
Arthur Ryman <arthur.ryman@gmail.com>
2019-11-17 03:12:24 -0500

    Added so5_cg.py

---
+++ so5_cg.py
@@ -0,0 +1,5 @@
+# This class implements the lookup of precomputed SO(5) > SO(3) Clebsch-Gordan coefficients.
+# These coefficients are precomputed by another program and saved in a set of files.
+# The files are arranged in a set of directories.
+# The file and directory names encode some of the parameters of the coefficients.
+# Each file contains a list of coefficient values, one per line, along with the remainder of their parameters.
